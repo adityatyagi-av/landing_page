2d77674cbd4021d96a94db4905fb682802b891f0
Aditya Tyagi <adityatyagiav1@gmail.com>
2023-01-28 17:34:04 +0530

    Feature section modified

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import './feature.css';
 
 
-function Feature() {
+function Feature({title,text}) {
   return (
-    <div>Feature</div>
+    <div className="features-container__feature">
+      <div className="features-container__feature-title">
+          <div/>
+          <h1>{title}</h1>
+      </div>
+      <div className="features-container_feature-text">
+        <p> {text}</p> 
+      </div>
+    </div>
   )
 }
 
