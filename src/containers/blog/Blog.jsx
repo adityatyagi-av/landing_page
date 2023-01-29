ae0b41c0afcbcd78d99c4a815cc4795646e4cfcc
Aditya Tyagi <adityatyagiav1@gmail.com>
2023-01-29 21:46:03 +0530

    blog container setup

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Article } from '../../components';
 import './blog.css'
+import {blog01,blog02,blog03,blog04,blog05} from './import'
+
 function Blog() {
   return (
-    <div>Blog</div>
+    <div className="blog section__padding" id="blog">
+      <div className="blog-heading">
+        <h1 className="gradient__text">A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className="blog-container">
+        <div className="blog-container_groupA">
+          <Article imgUrl={blog01} date="Jan 29, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+        </div>
+        <div className="blog-container_groupB">
+          <Article imgUrl={blog02} date="Jan 29, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imgUrl={blog03} date="Jan 29, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imgUrl={blog04} date="Jan 29, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imgUrl={blog05} date="Jan 29, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
