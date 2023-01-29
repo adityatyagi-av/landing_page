e1578cf3a6701d61c8c6e49447a00817f0a97f2d
Aditya Tyagi <adityatyagiav1@gmail.com>
2023-01-29 21:44:25 +0530

    Article component added

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './article.css'
 
-function Article() {
+function Article({imgUrl,date,title}) {
   return (
-    <div>Article</div>
+    <div className="blog-container_article">
+      <div className="blog-container_article-image">
+        <img src={imgUrl} alt='image'/>
+      </div>
+      <div className="blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read full article now..</p>
+      </div>
+    </div>
   )
 }
 
